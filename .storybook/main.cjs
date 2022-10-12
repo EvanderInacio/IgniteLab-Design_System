8deaeee7b84df6181dc33919d281a85c6cc9940a
Evander Inacio <evander.20112@hotmail.com>
2022-10-12 20:02:23 -0300

    ci: Add publish storybook workflow :boom:

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/IgniteLab-Design_System/'
+      config.base = '/ignite-lab-design-system/'
     }
 
     return config
