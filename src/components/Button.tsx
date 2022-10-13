1d2db9adec0e5d17d8188502f28ea21d6a30eed8
Evander Inacio <evander.20112@hotmail.com>
2022-10-12 23:53:00 -0300

    Web form :green_heart:

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,22 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChildren?: boolean;
 }
 
-export function Button({ children, asChildren }: ButtonProps) {
+export function Button({ children, asChildren, className, ...props}: ButtonProps) {
   const Comp = asChildren ? Slot : 'button'
 
   return (
     <Comp
       className={clsx(
-        "py-4 px-3 bg-cyan-500 rounded font-semibold text-black transition-colors focus:right-2 ring-white text-sm w-full hover:bg-cyan-300", 
+        "py-3 px-4 bg-cyan-500 rounded font-semibold text-black transition-colors focus:right-2 ring-white text-sm w-full hover:bg-cyan-300", 
+        className,
         )}
+        {...props}
     >
       {children}
     </Comp>
